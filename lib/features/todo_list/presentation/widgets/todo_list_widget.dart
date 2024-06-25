0caa3f8798a863e42b8a0613d7e0e779a72b4b34
Eugen Belyi <ebelyii@yandex.ru>
2024-06-26 00:40:09 +0500

    Dev[Fix AppBar]: Исправил проблему отрисовки AppBar с постоянно вылетающими исключениями
    Related to: Ada Lovelace

--- lib/features/todo_list/presentation/widgets/todo_list_widget.dart
+++ lib/features/todo_list/presentation/widgets/todo_list_widget.dart
@@ -114,6 +114,7 @@ class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
     double topTitlePadding = interpolate(100, 37, progress);
     double leftTitlePadding = interpolate(40, 20, progress);
     double topPositionSubtitle = interpolate(155, 37, progress);
+    double containerHeight = interpolate(maxExtent, minExtent, progress);
     double containerShadowOffset = interpolate(-7, 4, progress);
     double containerBlurRadius = interpolate(0, 17, progress);
     double containerSpreadRadius = interpolate(4, 0, progress);
@@ -122,82 +123,94 @@ class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
     if (subOpacity <= 0) subOpacity = 0;
     final bloc = context.read<ToDoListBloc>();
 
-    return BlocBuilder<ToDoListBloc, ToDoListState>(
-      builder: (context, state) {
-        if (state is TodoListSuccessState) {
-          return Container(
-            decoration: BoxDecoration(
-              color: AppColors.lightColorWhite,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black45,
-                  blurRadius: containerBlurRadius,
-                  spreadRadius: containerSpreadRadius,
-                  offset: Offset(0, containerShadowOffset)
-                )
-              ]
-            ),
-            child: Center(
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: topPositionSubtitle,
-                    left: leftTitlePadding,
-                    right: 20,
-                    child: Row(
-                      children: [
-                        Opacity(
-                          opacity: subOpacity,
-                          child: Text(
-                            'Выполнено - ${state.completedTasks}',
-                            style: TextStyle(
-                              fontSize: subtitleSize,
+    // обернул в center, поскольку возникала проблема с тем,
+    // что maxExtent 200, но paintExtent - 36.
+    // Иными словами, виджеты внутри делегата занимали не все доступное
+    // пространство, которое я им выделил. Гуглил долго, не нашел ответов,
+    // однако могу предположить, что хедеру мешал BlocBuilder
+    // в определении доступных размеров
+    return Center(
+      child: BlocBuilder<ToDoListBloc, ToDoListState>(
+        builder: (context, state) {
+          if (state is TodoListSuccessState) {
+            return Container(
+              height: containerHeight,
+              decoration: BoxDecoration(
+                color: AppColors.lightColorWhite,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black45,
+                    blurRadius: containerBlurRadius,
+                    spreadRadius: containerSpreadRadius,
+                    offset: Offset(0, containerShadowOffset)
+                  )
+                ]
+              ),
+              child: Center(
+                child: SizedBox(
+                  height: containerHeight,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: topPositionSubtitle,
+                        left: leftTitlePadding,
+                        right: 20,
+                        child: Row(
+                          children: [
+                            Opacity(
+                              opacity: subOpacity,
+                              child: Text(
+                                'Выполнено - ${state.completedTasks}',
+                                style: TextStyle(
+                                  fontSize: subtitleSize,
+                                ),
+                              ),
                             ),
+                            const Spacer(),
+                            IconButton(
+                                onPressed: () {
+                                  bloc.add(ChangeFilterEvent(
+                                      state.filter == TaskFilter.showAll
+                                          ? TaskFilter.showOnly
+                                          : TaskFilter.showAll));
+                                  DementiappLogger.infoLog(
+                                      'Added ChangeFilterEvent 1');
+                                },
+                                icon: state.filter == TaskFilter.showOnly
+                                    ? const Icon(
+                                        Icons.visibility,
+                                        size: 30,
+                                        color: AppColors.lightColorGrayLight,
+                                      )
+                                    : const Icon(
+                                        Icons.visibility_off,
+                                        size: 30,
+                                        color: AppColors.lightColorRed,
+                                      ))
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        top: topTitlePadding,
+                        left: leftTitlePadding,
+                        child: Text(
+                          'Мои дела',
+                          style: TextStyle(
+                            fontSize: titleSize,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        const Spacer(),
-                        IconButton(
-                            onPressed: () {
-                              bloc.add(ChangeFilterEvent(
-                                  state.filter == TaskFilter.showAll
-                                      ? TaskFilter.showOnly
-                                      : TaskFilter.showAll));
-                              DementiappLogger.infoLog(
-                                  'Added ChangeFilterEvent 1');
-                            },
-                            icon: state.filter == TaskFilter.showOnly
-                                ? const Icon(
-                                    Icons.visibility,
-                                    size: 30,
-                                    color: AppColors.lightColorGrayLight,
-                                  )
-                                : const Icon(
-                                    Icons.visibility_off,
-                                    size: 30,
-                                    color: AppColors.lightColorRed,
-                                  ))
-                      ],
-                    ),
-                  ),
-                  Positioned(
-                    top: topTitlePadding,
-                    left: leftTitlePadding,
-                    child: Text(
-                      'Мои дела',
-                      style: TextStyle(
-                        fontSize: titleSize,
-                        fontWeight: FontWeight.bold,
                       ),
-                    ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
-          );
-        } else {
-          return const CircularProgressIndicator();
-        }
-      },
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 
